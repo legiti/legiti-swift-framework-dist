80c0b00651bbee577312c8fa01a78350e68b64dc
Pearson Henri <pearsonhenri@gmail.com>
2019-04-02 12:23:26 -0300

    Add universal compiled framework

---
+++ InspetorSwiftFramework.framework/Headers/InspetorSwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  InspetorSwiftFramework.h
+//  InspetorSwiftFramework
+//
+//  Created by Pearson Henri on 3/21/19.
+//  Copyright © 2019 Inspetor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InspetorSwiftFramework.
+FOUNDATION_EXPORT double InspetorSwiftFrameworkVersionNumber;
+
+//! Project version string for InspetorSwiftFramework.
+FOUNDATION_EXPORT const unsigned char InspetorSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InspetorSwiftFramework/PublicHeader.h>
+
+
