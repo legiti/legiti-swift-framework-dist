c7c326e907ae310a7780001b63ac9471c5e8f89e
Lourenço Biselli <lourenco.biselli@gmail.com>
2020-09-22 13:11:57 -0300

    Testing the new xcframework

---
+++ Legiti.xcframework/ios-arm64_i386_x86_64-simulator/Legiti.framework/Headers/Legiti.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Inspetor.
+FOUNDATION_EXPORT double LegitiVersionNumber;
+
+//! Project version string for Inspetor.
+FOUNDATION_EXPORT const unsigned char LegitiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Inspetor/PublicHeader.h>
