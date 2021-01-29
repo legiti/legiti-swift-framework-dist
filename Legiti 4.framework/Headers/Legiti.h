fc19a78ecea552baa7a410a0548d10d1f42ad8db
Lourenço Biselli <lourenco.biselli@gmail.com>
2021-01-29 10:39:10 -0300

    New version 1.1.4

--- Legiti 4.framework/Headers/Legiti.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-//! Project version number for Inspetor.
-FOUNDATION_EXPORT double LegitiVersionNumber;
-
-//! Project version string for Inspetor.
-FOUNDATION_EXPORT const unsigned char LegitiVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Inspetor/PublicHeader.h>
